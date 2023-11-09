480a746ce0a68a1bec4dc8ada03a1eaebe70706d
victorvalentee <victorvalentee@gmail.com>
2023-11-09 08:59:28 -0300

    Habilitar todas as operações no run_project.sh

--- run_project.sh
+++ run_project.sh
@@ -16,23 +16,23 @@ fi
 
 # Remover arquivos de saída do ETL
 echo -e "\n\n${GREEN}Excluindo arquivos parquet criados em execuções prévias ...${NC}"
-# rm -rf ./etl/output
+rm -rf ./etl/output
 
 
 # Remover containers ativos gerados pelo docker-compose
 echo -e "${GREEN}Excluindo containers criados em execuções prévias pelo 'docker compose' ...${NC}\n\n"
-# docker compose down --remove-orphans --volumes
+docker compose down --remove-orphans --volumes
 
 
 # Construir imagens, baixar dependências e subir containers
 echo -e "\n\n${GREEN}Baixando e construindo as imagens docker ...${NC}\n\n"
-# docker compose build
+docker compose build
 
 
 # Subir containers e inicializar Airflow
 echo -e "\n----------------------------------\n\n${YELLOW}Inicalizando Airflow...${NC}"
 echo -e "${YELLOW}A DAG irá rodar automaticamente após o airflow-webserver ser inicializado.${NC}\n\n"
-# docker compose up -d
+docker compose up -d
 
 
 # Excluir containers após 1h
@@ -43,4 +43,4 @@ ${YELLOW}password: ${RED}airflow${NC}
 \n----------------------------------\n
 ${BLUE}Em alguns minutos os arquivos de saída serão automaticamente criados na pasta:${NC} ./etl/output/
 ${YELLOW}Os containers serão destruidos após ${RED}1 hora${YELLOW}, caso você não o faça manualmente.${NC}\n\n"
-# sleep 1h && docker compose down --remove-orphans --volumes
+sleep 1h && docker compose down --remove-orphans --volumes
