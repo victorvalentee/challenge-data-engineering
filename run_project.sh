1dbbd04db650422500e1a8e34f4c0a6a753d0478
victorvalentee <victorvalentee@gmail.com>
2023-11-09 08:55:42 -0300

    Formatação do script principal

--- run_project.sh
+++ run_project.sh
@@ -1,5 +1,6 @@
 # Color codes para output colorido no terminal
 RED='\033[0;31m'
+BLUE='\033[1;34m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
@@ -15,28 +16,31 @@ fi
 
 # Remover arquivos de saída do ETL
 echo -e "\n\n${GREEN}Excluindo arquivos parquet criados em execuções prévias ...${NC}"
-rm -rf ./etl/output
+# rm -rf ./etl/output
 
 
 # Remover containers ativos gerados pelo docker-compose
 echo -e "${GREEN}Excluindo containers criados em execuções prévias pelo 'docker compose' ...${NC}\n\n"
-docker compose down --remove-orphans --volumes
+# docker compose down --remove-orphans --volumes
 
 
 # Construir imagens, baixar dependências e subir containers
 echo -e "\n\n${GREEN}Baixando e construindo as imagens docker ...${NC}\n\n"
-docker compose build
+# docker compose build
 
 
 # Subir containers e inicializar Airflow
 echo -e "\n----------------------------------\n\n${YELLOW}Inicalizando Airflow...${NC}"
 echo -e "${YELLOW}A DAG irá rodar automaticamente após o airflow-webserver ser inicializado.${NC}\n\n"
-docker compose up -d
+# docker compose up -d
 
 
 # Excluir containers após 1h
-echo -e "\n----------------------------------\n\n${YELLOW}Daqui a alguns minutos você poderá acessar o dashboard do airflow no seguinte link:${NC}"
-echo "http://localhost:8080"
-echo -e "${YELLOW}user: ${RED}airflow${NC}\n${YELLOW}password: ${RED}airflow${NC}"
-echo -e "\n\n----------------------------------\n\n${YELLOW}Os containers serão destruidos após ${RED}1 hora${YELLOW}, caso você não o faça manualmente.${NC}\n\n"
-sleep 1h && docker compose down --remove-orphans --volumes
+echo -e "\n----------------------------------\n\n${YELLOW}Daqui a alguns minutos você poderá acessar o dashboard do airflow no seguinte link:${NC}
+http://localhost:8080
+${YELLOW}user: ${RED}airflow${NC}
+${YELLOW}password: ${RED}airflow${NC}
+\n----------------------------------\n
+${BLUE}Em alguns minutos os arquivos de saída serão automaticamente criados na pasta:${NC} ./etl/output/
+${YELLOW}Os containers serão destruidos após ${RED}1 hora${YELLOW}, caso você não o faça manualmente.${NC}\n\n"
+# sleep 1h && docker compose down --remove-orphans --volumes
